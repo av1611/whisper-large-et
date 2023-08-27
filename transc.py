d082edde10377187a42d4200d5b89618fcec3131
av1611 <alexej.ashkenazi@gmail.com>
2023-08-27 15:47:32 +0200

    further...

--- transc.py
+++ transc.py
@@ -1,12 +1,44 @@
 from transformers import pipeline
 import os
 
+
 # Initialize the Whisper pipeline with your custom model
 # whisper_pipeline = pipeline("automatic-speech-recognition", model="path_to_your_custom_model")
 # whisper_pipeline = pipeline("automatic-speech-recognition", model="TalTechNLP/whisper-large-et")
 # whisper_pipeline = pipeline("automatic-speech-recognition", model="arampacha/whisper-large-uk-2")
-whisper_pipeline = pipeline("automatic-speech-recognition", model="Yehor/whisper-small-ukrainian")
-# Path to the directory containing MP3 files
+# whisper_pipeline = pipeline("automatic-speech-recognition", model="Yehor/whisper-small-ukrainian")
+whisper_pipeline = pipeline("automatic-speech-recognition",
+    model="Yehor/wav2vec2-xls-r-1b-uk-with-lm")
+
+# whisper_pipeline = pipeline("automatic-speech-recognition", model="Yehor/wav2vec2-xls-r-300m-uk-with-news-lm")
+# whisper_pipeline = pipeline("automatic-speech-recognition", model="Yehor/wav2vec2-xls-r-300m-uk-with-news-lm")
+# whisper_pipeline = pipeline("automatic-speech-recognition", model="Yehor/wav2vec2-xls-r-300m-uk-with-news-lm")
+# whisper_pipeline = pipeline("automatic-speech-recognition", model="Yehor/wav2vec2-xls-r-300m-uk-with-news-lm")
+
+# Yehor/bn-base # retnd error on the 1st run, mbpi, m1
+
+# Yehor/wav2vec2-xls-r-base-uk-with-cv-lm # 2h processed in 15 min
+# Yehor/wav2vec2-xls-r-base-uk-with-small-lm # 2h processed in 15 min
+
+# Yehor/wav2vec2-xls-r-300m-uk # # retnd error on the 1st run, m1
+# Yehor/wav2vec2-xls-r-300m-uk-with-lm # 2h processed in 20 min
+# Yehor/wav2vec2-xls-r-300m-uk-with-small-lm # 2h processed in 20 min
+# Yehor/wav2vec2-xls-r-300m-uk-with-small-lm-noisy # 2h processed in 30 min
+
+# Yehor/wav2vec2-xls-r-300m-uk-with-news-lm # 2h processed in 30 min
+# Yehor/wav2vec2-xls-r-300m-uk-with-wiki-lm # 2h processed in 25 min
+# Yehor/wav2vec2-xls-r-300m-uk-with-3gram-news-lm # 2h processed in 25 min
+# Yehor/wav2vec2-xls-r-300m-uk-traced-jit # err, faild downld
+
+# Yehor/wav2vec2-xls-r-1b-uk-with-lm # OK
+# Yehor/wav2vec2-xls-r-1b-uk-with-news-lm
+# Yehor/wav2vec2-xls-r-1b-uk-with-binary-news-lm
+
+# Yehor/whisper-small-ukrainian
+
+# Yehor/kenlm-ukrainian
+# Yehor/indonesian-kenlm-newspapers
+
 audio_dir = "output_chunks"
 
 # Output file to save transcribed text
